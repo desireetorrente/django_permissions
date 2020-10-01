4ce4e25084fab11245ce9cdde158322ed8c1c228
Jorge <jm@botxo.co>
2020-10-01 08:47:03 +0200

    Filter permissions in admin

--- permissions/learning/admin.py
+++ permissions/learning/admin.py
@@ -2,13 +2,40 @@ from django.contrib import admin
 
 from .models import Bot, Company, UserProfile
 from guardian.admin import GuardedModelAdmin
+from guardian.shortcuts import get_objects_for_user
+from django.contrib import messages
 
 
 class BotAdmin(GuardedModelAdmin):
-    list_display = ('name', )
+    list_display = ('name', 'company', 'created_by' )
+    actions = ['make_published']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return get_objects_for_user(request.user, 'learning.view_bot', qs, accept_global_perms=False)
+
+    def make_published(self, request, queryset):
+        if request.user.has_perm('learning.bot_publish'):
+            updated = get_objects_for_user(request.user, 'learning.bot_publish', queryset)
+            print(updated)
+            self.message_user(request,
+                'Selected bots successfully published.',
+                messages.SUCCESS)
+        else:
+            self.message_user(request,
+                f'You are not allowed to perform this action',
+                messages.ERROR)
+
+    make_published.short_description = "Publish bot"
+
+
 
 class CompanyAdmin(GuardedModelAdmin):
     list_display = ('name', )
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return get_objects_for_user(request.user, 'learning.view_company', qs, accept_global_perms=False)
 
 # Register your models here.
 admin.site.register(Bot, BotAdmin)
